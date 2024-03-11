5bb7d6c5de67fb597192e547959d26f0d34bf782
anzilabdulhakkim <anzilabdulhakkim135@gmail.com>
2024-03-12 00:56:18 +0530

    added data.js

---
+++ src/data.js
@@ -0,0 +1,13 @@
+export const API_KEY='AIzaSyAj-j2GFOs7btf8n8VZEfL2aoMyhJ7oxP0'
+
+export const value_converter=(value)=>{
+if(value>=1000000){
+    return Math.floor(value/1000000)+"M";
+}
+else if(value>=1000){
+    return Math.floor(value/1000)+"K";
+}
+else{
+    return value
+}
+}
