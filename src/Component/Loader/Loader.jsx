6faf260e8f437780f5784f4ebc99ab4032d03ef3
anzilabdulhakkim <anzilabdulhakkim135@gmail.com>
2024-03-12 01:02:43 +0530

    added loader component

---
+++ src/Component/Loader/Loader.jsx
@@ -0,0 +1,13 @@
+import { Load } from "./Style.jsx";
+
+const Loader = () => {
+    return (
+        <div>
+        <Load>
+            <iframe src="https://giphy.com/embed/wbqZ0b2olKBFDEIHiQ" width="480" height="216" frameBorder="0" allowFullScreen></iframe>
+        </Load>
+        </div>
+    );
+};
+
+export default Loader;
